cf6301e5c8ebb492072a41c860c40eafd16a3add
ly0unsi <mcabdo25@gmail.com>
2021-09-23 14:09:39 +0100

    commit

---
+++ app/Http/Controllers/Api/SupplierController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Model\Supplier;
+use Intervention\Image\Facades\Image;
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $suppliers = Supplier::all();
+        return response()->json($suppliers);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'name' => 'required|unique:suppliers|max:255',
+            'email' => 'required',
+            'phone' => 'required|unique:suppliers',
+
+        ]);
+        $data = array();
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['phone'] = $request->phone;
+        $data['shopname'] = $request->shopname;
+        $data['address'] = $request->address;
+
+        if ($request->photo) {
+            $position = strpos($request->photo, ';');
+            $sub = substr($request->photo, 0, $position);
+            $ext = explode('/', $sub)[1];
+
+            $name = time() . "." . $ext;
+            $img = Image::make($request->photo)->resize(240, 200);
+            $upload_path = 'backend/supplier/';
+            $image_url = $upload_path . $name;
+            $img->save($image_url);
+
+            $data['photo'] = $image_url;
+            Supplier::create($data);
+        } else {
+
+            Supplier::create($data);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $supplier = Supplier::where('id', $id)->first();
+        return response()->json($supplier);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = array();
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['phone'] = $request->phone;
+        $data['shopname'] = $request->shopname;
+        $data['address'] = $request->address;
+
+        $image = $request->newphoto;
+
+        if ($image) {
+            $position = strpos($image, ';');
+            $sub = substr($image, 0, $position);
+            $ext = explode('/', $sub)[1];
+
+            $name = time() . "." . $ext;
+            $img = Image::make($image)->resize(240, 200);
+            $upload_path = 'backend/supplier/';
+            $image_url = $upload_path . $name;
+            $success = $img->save($image_url);
+
+            if ($success) {
+                $data['photo'] = $image_url;
+                $img = Supplier::where('id', $id)->first();
+                $image_path = $img->photo;
+                $done = unlink($image_path);
+                $user  = Supplier::where('id', $id)->update($data);
+            }
+        } else {
+            $oldphoto = $request->photo;
+            $data['photo'] = $oldphoto;
+            $user = Supplier::where('id', $id)->update($data);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $supplier = Supplier::where('id', $id)->first();
+        $photo = $supplier->photo;
+        if ($photo) {
+            unlink($photo);
+            Supplier::where('id', $id)->delete();
+        } else {
+            Supplier::where('id', $id)->delete();
+        }
+    }
+}
---
+++ resources/js/components/salary/index.vue
@@ -0,0 +1,171 @@
+
+
+<template>
+
+  <div>
+
+    <div class="row col-md-12">
+        <div class="col-md-6">
+            <router-link to="/given-salary" class="btn btn-primary float-left">Pay an employee </router-link>
+        </div>
+        <div class="col-md-6 float-right">
+            <input type="text" v-model="searchTerm" class="form-control float-right" style="width: 300px;" placeholder="Search Here">
+        </div>
+    </div>
+
+<br>
+   <div class="row" >
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-4 text-center">
+              <!-- Simple Tables -->
+              <div class="card text-center">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">All Salary Details</h6>
+                </div>
+                <div class="table-responsive">
+                  <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                      <tr>
+                        <th>Month Name</th>
+                        <th>Details</th>
+
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="salary in filtersearch.slice(0, 4)" :key="salary.id">
+                        <td> {{ salary.salary_month }} </td>
+
+                        <td>
+                            <router-link :to="{name: 'view-salary', params:{id:salary.salary_month}}" class="btn btn-sm btn-primary">View Salary</router-link>
+                        </td>
+                      </tr>
+
+                    </tbody>
+                  </table>
+                </div>
+                <div class="card-footer"></div>
+              </div>
+            </div>
+            <!-- Card #2 -->
+
+            <div v-if="filtersearch.length>=4" class="col-lg-4 col-md-6 col-sm-12 mb-4 text-center">
+              <!-- Simple Tables -->
+              <div class="card text-center">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">...</h6>
+                </div>
+                <div class="table-responsive">
+                  <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                      <tr>
+                        <th>Month Name</th>
+                        <th>Datails</th>
+
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="salary in filtersearch.slice(4, 8)" :key="salary.id">
+                        <td> {{ salary.salary_month }} </td>
+
+                        <td>
+                            <router-link :to="{name: 'view-salary', params:{id:salary.salary_month}}" class="btn btn-sm btn-primary">View Salary</router-link>
+                        </td>
+                      </tr>
+
+                    </tbody>
+                  </table>
+                </div>
+                <div class="card-footer"></div>
+              </div>
+            </div>
+            <!-- Card #2 -->
+
+            <!-- Card #3 -->
+
+            <div v-if="filtersearch.length>8" class="col-lg-4 col-md-6 col-sm-12 mb-4 text-center">
+              <!-- Simple Tables -->
+              <div class="card text-center">
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">...</h6>
+                </div>
+                <div class="table-responsive">
+                  <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                      <tr>
+                        <th>Month Name</th>
+                        <th>Datails</th>
+
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="salary in filtersearch.slice(8, 12)" :key="salary.id">
+                        <td> {{ salary.salary_month }} </td>
+
+                        <td>
+                            <router-link :to="{name: 'view-salary', params:{id:salary.salary_month}}" class="btn btn-sm btn-primary">View Salary</router-link>
+                        </td>
+                      </tr>
+
+                    </tbody>
+                  </table>
+                </div>
+                <div class="card-footer"></div>
+              </div>
+            </div>
+            <!-- Card #3 -->
+
+          </div>
+          <!--Row-->
+
+</div>
+
+
+
+</template>
+
+
+
+<script type="text/javascript">
+
+  export default {
+    created(){
+      if (!User.loggedIn()) {
+        this.$router.push({name: '/'})
+      }
+    },
+    data(){
+      return{
+        salaries:[],
+        searchTerm:''
+      }
+    },
+    computed:{
+      filtersearch(){
+      return this.salaries.filter(salary => {
+         return salary.salary_month.match(this.searchTerm)
+      })
+      }
+    },
+
+  methods:{
+    allSalary(){
+      axios.get('/api/salary/')
+      .then(({data}) => (this.salaries = data))
+      .catch()
+    },
+
+  },
+  created(){
+    this.allSalary();
+  }
+
+
+  }
+</script>
+
+
+<style type="text/css">
+  #em_photo{
+    height: 40px;
+    width: 40px;
+  }
+</style>
